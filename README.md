75b9f38a14665d0a39e5e08cf80a43704a1899a0
Manjit-karki <manjitkarki05@gmail.com>
2025-05-07 22:40:40 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+<p align="center">
+  This repo is the collection of all java codes starting from basics and moving up in difficulty as the course progresses.
+  Basic outputs for each new code will be put here as an image along with the codes beside it. 
+</p>
+
+<p align="center">
+  <img src="outputs/output1.png" alt="output" width="600"/>
+</p>
---
+++ mainClass.java
@@ -0,0 +1,43 @@
+// INFO: Please Uncomment the codes to run them.
+// Advance Programmingin Java, A college lab report.
+
+
+
+
+// INFO: Biasic arithmetic:
+
+
+// import java.util.Scanner;
+//
+// class mainClass{
+//
+//   public static void main(String[] args) {
+//     Scanner scanner = new Scanner(System.in);
+//
+//     System.out.println("Enter two numbers:");
+//     int a = scanner.nextInt();
+//     int b = scanner.nextInt();
+//
+//     System.out.println("The sum of "+ a +" and "+ b +" is "+ (a+b));
+//     System.out.println("The subtraction of "+ a +" and "+ b +" is "+ (a-b));
+//
+//     if (a!=0 && b!=0)
+//     {
+//       System.out.println("The Multiplicaiton of "+ a +" and "+ b +" is "+ (float)a*b);
+//     }
+//     else {
+//       System.out.println("The Multiplicaiton of two numbers is 0.");
+//     }
+//
+//     if (a!=0) {
+//       System.out.println("The division of "+ b +" and "+ a +" is "+ (float)b/a);
+//     } else {
+//       System.out.println("One or both of the give numbers are zero.");
+//     }
+//
+//
+//     scanner.close();
+//   }
+// }
+
+
